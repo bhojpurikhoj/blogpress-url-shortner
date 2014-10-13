7b02b7792974b7369fef74ef38ddde0227e3ceb7
Bhojpurikhoj.Com <bhojpurikhoj@gmail.com>
2014-10-13 14:05:05 +0530

    Create ReadMe.php

---
+++ ReadMe.php
@@ -0,0 +1,3 @@
+1.Please changed config.php with databse, and 
+2.If you like administration controll,installer,edit and delete option by admin.
+3.Blogpress Url Shortner Pro has been buy at http://bhojpurikhoj.com/shop (Advanced Feature)

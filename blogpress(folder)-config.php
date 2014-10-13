1b1f606cfd6a6aebc8f4ff1ef94dbb582ba16265
Bhojpurikhoj.Com <bhojpurikhoj@gmail.com>
2014-10-13 14:01:26 +0530

    Update blogpress(folder)-config.php

--- blogpress(folder)-config.php
+++ blogpress(folder)-config.php
@@ -2,9 +2,9 @@
 
 //mysql db connection information
 $hostname = "localhost"; //host
-$database = "blogpress"; //database
-$username = "url"; //username for your database
-$password = "9012243013"; //password for your database
+$database = ""; //database
+$username = ""; //username for your database
+$password = ""; //password for your database
 
 $site = mysql_connect($hostname, $username, $password); 
 mysql_select_db($database, $site);
